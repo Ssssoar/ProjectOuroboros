ee7b7f150b8bb83ca3734bb281d34ab840c174bc
Ssssoar <jano20@vtr.net>
2024-10-29 17:43:56 -0300

    More figuring out is happening

--- Assets/Scripts/SsoarScripts/SCR_HoldAndCamera.cs
+++ Assets/Scripts/SsoarScripts/SCR_HoldAndCamera.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Events;
+using Cinemachine;
 
 
 public class SCR_HoldAndCamera : MonoBehaviour{
     [Header("References")]
     [SerializeField] PlayableDirector director;
     [SerializeField] UnityEvent onUnreadyRelease;
+    [SerializeField] CinemachineVirtualCamera defaultCam , focusCam;
 
     bool readied = false;
 
     void OnMouseDown(){
         if (readied) return;
+        focusCam.Priority = 10;
+        defaultCam.Priority = 0;
         director.Play();
     }
 
     void OnMouseUp(){
         if (readied) return;
+        focusCam.Priority = 0;
+        defaultCam.Priority = 10;
         director.Stop();
         onUnreadyRelease?.Invoke();
     }
