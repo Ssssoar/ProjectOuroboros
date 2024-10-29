f11af98909a9a16690b553f9aaf5200353fe4ac0
Ssssoar <jano20@vtr.net>
2024-10-29 16:24:36 -0300

    Beginnings of hold element

---
+++ Assets/Scripts/SsoarScripts/SCR_HoldAndCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_HoldAndCamera : MonoBehaviour{
+    void OnMouseDown(){
+        Debug.Log("pressed");
+    }
+
+    void OnMouseUp(){
+        Debug.Log("Released");
+    }
+}
